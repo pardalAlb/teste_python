f20817accfa8f3b537a315f76123be0e1a3bd7cf
Celso Furtado <celso.alunos@cfp127aluno.local>
2024-02-21 14:52:12 -0300

    Versão 1 do projeto

---
+++ dias_da_semana.py
@@ -0,0 +1,18 @@
+dia = int(input("Digite um número entre 0 e 6:"))
+
+if dia == 0:
+    print("Hoje é Domingo!")
+elif dia == 1:
+    print("Hoje é Segunda-feira!")
+elif dia == 2:
+    print("Hoje é Terça-feira!")
+elif dia == 3:
+    print("Hoje é Quarta-feira!")
+elif dia == 4:
+    print("Hoje é Quinta-feira!")
+elif dia == 5:
+    print("Hoje é Sexta-feira!")
+elif dia == 6:
+    print("Hoje é Sábado!")
+else:
+    print("Você é burro cara?")
---
+++ idade.py
@@ -0,0 +1,10 @@
+idade = 18
+
+if idade < 18:
+    nome = input("Qual o seu nome? ")
+    print(f"{nome}, você é MENOR de idade!")
+else:
+    print("Você é MAIOR de idade!") 
+
+print("  Fim do programa")
+
